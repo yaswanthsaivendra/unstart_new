f0e95e99fcb778d3ff60c9a5bdabc4a4376c51a5
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-05-22 21:27:39 +0530

    models and views created for announcements

--- manager/admin.py
+++ manager/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Contact,mainpagenews,testimonials,mainwebsite,news,events,announcement
+from .models import Contact,mainpagenews,testimonials,mainwebsite,news,events,Announcement
 
 # Register your models here.
 
@@ -8,5 +8,5 @@ admin.site.register(mainpagenews)
 admin.site.register(testimonials)
 admin.site.register(mainwebsite)
 admin.site.register(events)
-admin.site.register(announcement)
+admin.site.register(Announcement)
 admin.site.register(news)
