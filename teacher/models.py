f94350d1a1b8d5f82388ff4dd2ca73d546614d13
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-06-21 21:58:05 +0530

    added course detail and course learning pages

--- teacher/models.py
+++ teacher/models.py
@@ -41,6 +41,26 @@ class Unit(models.Model):
         self.slug = slugify(self.title + self.course.title)
         super(Unit, self).save(*args, **kwargs)
 
+    @property
+    def lessons_completed(self):
+        lesson_ids = self.lesson_set.values_list('id', flat=True)
+        completed_lesson_count = LessonProgress.objects.filter(
+            enrollment__student=self.request.user,
+            lesson__in=lesson_ids,
+            is_completed=True
+        ).count()
+        return completed_lesson_count
+    
+    @property
+    def total_lessons(self):
+        return self.lesson_set.count()
+    
+    @property
+    def completion_percentage(self):
+        if self.total_lessons > 0:
+            return (self.lessons_completed / self.total_lessons) * 100
+        return 0
+
 
 class Lesson(models.Model):
     lesson_number = models.IntegerField(null=False, default=1)
